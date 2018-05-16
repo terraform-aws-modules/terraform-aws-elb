520bed22370bfb1ad909cffb26b9866ffb1ce255
Anton Babenko <anton@antonbabenko.com>
2018-05-16 13:14:48 +0200

    Fixed outputs of ELB module when resource does not exist

--- modules/elb/outputs.tf
+++ modules/elb/outputs.tf
@@ -1,35 +1,35 @@
 # ELB
 output "this_elb_id" {
   description = "The name of the ELB"
-  value       = "${aws_elb.this.id}"
+  value       = "${element(concat(aws_elb.this.*.id, list("")), 0)}"
 }
 
 output "this_elb_arn" {
   description = "The ARN of the ELB"
-  value       = "${aws_elb.this.arn}"
+  value       = "${element(concat(aws_elb.this.*.arn, list("")), 0)}"
 }
 
 output "this_elb_name" {
   description = "The name of the ELB"
-  value       = "${aws_elb.this.name}"
+  value       = "${element(concat(aws_elb.this.*.name, list("")), 0)}"
 }
 
 output "this_elb_dns_name" {
   description = "The DNS name of the ELB"
-  value       = "${aws_elb.this.dns_name}"
+  value       = "${element(concat(aws_elb.this.*.dns_name, list("")), 0)}"
 }
 
 output "this_elb_instances" {
   description = "The list of instances in the ELB"
-  value       = ["${aws_elb.this.instances}"]
+  value       = "${element(concat(aws_elb.this.*.instances, list("")), 0)}"
 }
 
 output "this_elb_source_security_group_id" {
   description = "The ID of the security group that you can use as part of your inbound rules for your load balancer's back-end application instances"
-  value       = "${aws_elb.this.source_security_group_id}"
+  value       = "${element(concat(aws_elb.this.*.source_security_group_id, list("")), 0)}"
 }
 
 output "this_elb_zone_id" {
   description = "The canonical hosted zone ID of the ELB (to be used in a Route 53 Alias record)"
-  value       = "${aws_elb.this.zone_id}"
+  value       = "${element(concat(aws_elb.this.*.zone_id, list("")), 0)}"
 }
